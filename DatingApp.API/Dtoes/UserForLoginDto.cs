e50ba7b2c925975343f369e3315d3290d1299cc6
Qasim Albahdur <qasim.albahdour@omsgd.gov.om>
2019-12-19 12:29:39 +0400

    before weekend

---
+++ DatingApp.API/Dtoes/UserForLoginDto.cs
@@ -0,0 +1,11 @@
+namespace DatingApp.API.Dtoes
+{
+    public class UserForLoginDto
+    {
+        public string Username  { get; set; }
+
+        
+       
+        public string Password { get; set; }
+    }
+}
