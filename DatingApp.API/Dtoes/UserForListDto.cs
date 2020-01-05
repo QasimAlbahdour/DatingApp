113434c00a84bdfd17dc70cb65086d6ed56ef176
Qasim Albahdur <qasim.albahdour@omsgd.gov.om>
2020-01-05 13:25:41 +0400

    After extending the API

---
+++ DatingApp.API/Dtoes/UserForListDto.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace DatingApp.API.Dtoes
+{
+    public class UserForListDto
+    {
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        
+        public string Gender { get; set; }
+        public int age { get; set; } 
+        public string KnownAs { get; set; }
+       
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+       
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public string PhotoUrl { get; set; }
+        
+    }
+}
